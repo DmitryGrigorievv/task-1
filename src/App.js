2a0131d66d7b3cea3499f1d9695ed5141dca4a0f
DmitryGrigorievv <158502117+DmitryGrigorievv@users.noreply.github.com>
2024-09-26 23:52:39 +0500

    added count

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react';
 function App() {
+  const [count, setCount] = useState(0);
+  function increment() {
+    setCount(count+1);
+    console.log(count);
+  }
+  function decrement() {
+    setCount(count-1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <button onClick={increment}>Увеличить</button>
+      <button onClick={decrement}>Уменьшить</button>
     </div>
   );
 }
