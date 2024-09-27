5fc90433b7273e03ac49278b1a426848136c69ab
Aleksei Sidenko <sidoi90@bk.ru>
2024-09-27 22:37:32 +0500

    add mock data for future work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+
 function App() {
+
   const [count, setCount] = useState(0);
+
   function increment() {
-    setCount(count+1);
-    console.log(count);
+    setCount(count + 1);
   }
+
   function decrement() {
-    setCount(count-1);
+    setCount(count - 1);
   }
+
   return (
     <div className="App">
       <h1>{count}</h1>
-      <button onClick={increment}>Увеличить</button>
       <button onClick={decrement}>Уменьшить</button>
+      <button onClick={increment}>Увеличить</button>
     </div>
   );
 }
---
+++ src/mockData.js
@@ -0,0 +1,226 @@
+export const data = [
+    {
+        "id": 1402937,
+        "name": "Артур, ты король",
+        "description": "Это было последнее соревнование капитана по приключенческим гонкам Майкла Лайта, он был полон решимости ничему не позволить встать у него на пути. Собрав первоклассную команду, он не мог и представить, что в 700-километровом забеге у них появится неожиданный попутчик — пес по кличке Артур, встреча с которым изменит не только исход гонки, но и их жизнь.",
+        "shortDescription": "Во время экстремальной гонки Майкл находит друга — бездомного пса. Приключенческий фильм с Марком Уолбергом",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/10809116/02f5c9fa-8ed2-455b-9861-1a01252d73f4/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/10809116/02f5c9fa-8ed2-455b-9861-1a01252d73f4/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/223007/2a0000018eaa2f034fc72de007fe2ac7678d/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/223007/2a0000018eaa2f034fc72de007fe2ac7678d/x1000"
+        }
+    },
+    {
+        "id": 5105855,
+        "name": "Одна жизнь",
+        "description": "Скромного и непубличного джентльмена Николаса Уинтона приглашают на телевизионное ток-шоу, где он наконец-то узнает всю правду о событиях Второй мировой войны, когда он пытался спасти сотни детей от гибели в концлагере. Одна жизнь может изменить судьбы сотен людей — как в прошлом, так и в настоящем.",
+        "shortDescription": "Молодой филантроп придумывает, как спасти сотни детей от концлагеря. Проникновенная драма с Энтони Хопкинсом",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/9784475/4564801f-fe39-4493-b042-98038357d242/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/9784475/4564801f-fe39-4493-b042-98038357d242/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/236744/2a0000018e9a4990ede987ce5ae3b643a2f9/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/236744/2a0000018e9a4990ede987ce5ae3b643a2f9/x1000"
+        }
+    },
+    {
+        "id": 5275429,
+        "name": "Лёд 3",
+        "description": "Надя выросла и стала фигуристкой. Она мечтает о «Кубке льда», как когда-то мечтала ее мама. Горин возражает против спортивной карьеры дочери — он оберегает ее от любых трудностей. На тайной тренировке Надя знакомится с молодым и дерзким хоккеистом из Москвы, и между ними вспыхивает первая любовь. Отец не верит в искренность чувств юноши и разлучает пару.",
+        "shortDescription": "Надя идет наперекор отцу ради любви и большого спорта. Продолжение любимой истории с Александром Петровым",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/10671298/5d4e5068-4902-4dd3-a318-c13272de7134/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/10671298/5d4e5068-4902-4dd3-a318-c13272de7134/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/1652588/2a0000018eadd38f83d1618858084599c213/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/1652588/2a0000018eadd38f83d1618858084599c213/x1000"
+        }
+    },
+    {
+        "id": 1108571,
+        "name": "Приключения Реми",
+        "description": "Удивительное путешествие по Франции маленького Реми в компании уличного музыканта, обезьянки и цирковой собаки.  Вместе им предстоит пережить неожиданные встречи, приключения и испытания, чтобы раскрыть тайну происхождения мальчика.",
+        "shortDescription": "Мальчик Реми и уличный артист Виталис бродят по Франции XIX века. Красивая авантюрная мелодрама для всей семьи",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/1599028/3bb8ef0f-0230-4eeb-acb0-e2b44329de37/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/1599028/3bb8ef0f-0230-4eeb-acb0-e2b44329de37/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/1652588/2a0000016d1fad5a7fa59ff75917c900760d/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/1652588/2a0000016d1fad5a7fa59ff75917c900760d/x1000"
+        }
+    },
+    {
+        "id": 7466,
+        "name": "Бронкская история",
+        "description": "Король бронкской мафии Сонни убил человека. Просто так. Из-за места на парковке. Среди бела дня вытащил пистолет и несколько раз выстрелил. Полиция узнала, что свидетелем преступления был 9-летний Калоджеро, и допросила мальчика. Но Калоджеро не выдал Сонни. Так началась их дружба. И когда Калоджеро вырос, он не просто стал свидетелем других кровавых деяний своего босса, но и сам оказался на краю пропасти.",
+        "shortDescription": "Юный свидетель убийства не выдает преступника. Режиссерский дебют Роберта де Ниро о взрослении на злых улицах",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/4483445/a97b1c64-8726-4ec8-be0e-87a12ef717b8/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/4483445/a97b1c64-8726-4ec8-be0e-87a12ef717b8/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/239697/2a0000016c20737facd44a56f7b55e93e3ca/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/239697/2a0000016c20737facd44a56f7b55e93e3ca/x1000"
+        }
+    },
+    {
+        "id": 463694,
+        "name": "Землетрясение",
+        "description": "28 июля 1976 года в Таншане провинции Хэбэй произошло землетрясение, которое длилось меньше чем полминуты, но унесло несколько сотен тысяч жизней. Эти секунды поставили мать перед ужасным выбором — спасти сына или дочь, что обернулось грузом вины на десятилетия.",
+        "shortDescription": "В момент природной катастрофы матери приходится делать тяжелый выбор. Драма Фэн Сяогана по реальным событиям",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/4774061/42571afc-b3f9-4f95-915d-00acd154e6b4/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/4774061/42571afc-b3f9-4f95-915d-00acd154e6b4/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/223007/2a00000184613341904276d0e7aec9ead403/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/223007/2a00000184613341904276d0e7aec9ead403/x1000"
+        }
+    },
+    {
+        "id": 425400,
+        "name": "Пожары",
+        "description": "Когда брат и сестра Марван ознакомились с завещанием матери, их удивлению не было предела. Её последней волей было разыскать отца, которого близнецы считали умершим, и брата, о существовании которого они даже не подозревали. Жанна и Симон узнают, что мать тщательно скрывала от них многие обстоятельства своей трагической судьбы.",
+        "shortDescription": "Брат и сестра узнают о страшном прошлом умершей матери. Драма Дени Вильнёва о жертвах ближневосточных войн",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/4303601/40b4b961-07ab-4ce3-892f-63d6bd1fabc6/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/4303601/40b4b961-07ab-4ce3-892f-63d6bd1fabc6/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/1534341/2a0000017fff5812f055d71d941aeac64090/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/1534341/2a0000017fff5812f055d71d941aeac64090/x1000"
+        }
+    },
+    {
+        "id": 4384109,
+        "name": "Белая птица: Новое чудо",
+        "description": "Джулиана Олбанса исключили из школы за жестокое обращение с Огги Пуллманом. К Джулиану приезжает бабушка, которая делится с внуком историей ее юности в оккупированной нацистами Франции. Тогда ее жизнь спасли только доброта и мужество почти незнакомого ей юноши, которого также донимали в школе одноклассники.",
+        "shortDescription": "Юноша-изгой укрывает еврейскую девочку от нацистов. Пронзительная драма с Джиллиан Андерсон",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/10671298/4d35113c-a287-415e-82b6-e16c6e19107e/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/10671298/4d35113c-a287-415e-82b6-e16c6e19107e/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/12808873/2a0000018e66a78c72d4408791551d899d3f/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/12808873/2a0000018e66a78c72d4408791551d899d3f/x1000"
+        }
+    },
+    {
+        "id": 610,
+        "name": "Билли Эллиот",
+        "description": "Чем должен увлекаться сын шахтера? Сомнений нет: боксом и только боксом. Но у 11-летнего Билли Эллиота на этот счет было свое мнение — он обожает балет.\n\nОтец и старший брат Тони впадают в ярость, когда узнают, что он променял бокс на «менее мужественный» балет. И только острая на язык миссис Уилкинсон поддерживает Билли в его желании танцевать, и он получает шанс поступить в Королевскую балетную школу.",
+        "shortDescription": "Сын шахтера втайне от отца занимается балетом. Вдохновляющая драма с Джейми Беллом о победе над предрассудками",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/10671298/9d509077-7025-4875-88f0-f9c3e7e4cca7/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/10671298/9d509077-7025-4875-88f0-f9c3e7e4cca7/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/2439731/2a0000018ab72e9b8f080969270458511aa8/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/2439731/2a0000018ab72e9b8f080969270458511aa8/x1000"
+        }
+    },
+    {
+        "id": 4395219,
+        "name": "Мой Хатико",
+        "description": "Чунцин, начало 2000-х годов. Во время поездки за город с коллегами немолодой университетский преподаватель Чэнь подбирает на дороге грязного и испуганного щенка. Никто не соглашается взять пёсика себе, поэтому мужчине приходится втихаря принести его домой, хотя супруга терпеть не может собак. Пообещав, что найдёт щенку новый дом, Чэнь тем не менее отказывает всем претендентам — так пёс получает кличку Хатико и становится новым членом семьи.",
+        "shortDescription": "Пожилой преподаватель заводит щенка вопреки протестам жены. Безмерно трогательная история верности из Китая",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/9784475/50e2559d-c29c-494b-b729-3961352e20bc/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/9784475/50e2559d-c29c-494b-b729-3961352e20bc/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/tmdb-images/original/kGWpaisyiOrOhkjn5FviMRUaoCb.jpg",
+            "previewUrl": "https://image.openmoviedb.com/tmdb-images/w500/kGWpaisyiOrOhkjn5FviMRUaoCb.jpg"
+        }
+    },
+    {
+        "id": 1103852,
+        "name": "Укрась прощальное утро цветами обещания",
+        "description": "Красавица Макия — из рода бессмертных. На протяжении веков многие армии пытались захватить её народ, чтобы завладеть секретом вечной жизни. И вот теперь, когда город разрушен, девушка прячется в лесу. Здесь она находит потерявшего родителей маленького мальчика Эриала и начинает о нём заботиться.",
+        "shortDescription": "Может ли бессмертная стать матерью чужому ребенку? Фэнтези-аниме — режиссерский дебют сценаристки Мари Окады",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/1599028/551571aa-2c50-41b2-9ffb-51878e443054/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/1599028/551571aa-2c50-41b2-9ffb-51878e443054/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/224348/2a000001660c28ec0777e0c547fccf7c94f8/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/224348/2a000001660c28ec0777e0c547fccf7c94f8/x1000"
+        }
+    },
+    {
+        "id": 3707,
+        "name": "Страсти Христовы",
+        "description": "Попытка детально воссоздать последние двенадцать часов из жизни Иисуса Христа.",
+        "shortDescription": "Последние часы жизни Спасителя на Земле. Бескомпромиссная драма Мэла Гибсона, взорвавшая мировой прокат",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/10835644/1b7bc6cf-055e-43e3-b321-359e7a6db0f5/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/10835644/1b7bc6cf-055e-43e3-b321-359e7a6db0f5/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/1531675/2a0000018d46ecbe8ed4bff5236c50c0078f/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/1531675/2a0000018d46ecbe8ed4bff5236c50c0078f/x1000"
+        }
+    },
+    {
+        "id": 45822,
+        "name": "Вокзал для двоих",
+        "description": "Город Заступинск, где-то между Москвой и Алма-Атой, ближе к Воронежу. Вокзальная буфетчица Вера и пианист Платон Рябинин из Москвы познакомились при весьма непривлекательных обстоятельствах. В результате Вера потеряла жениха с дынями, но нашла любимого, который вскоре должен отбыть в отдаленные места, чтобы нести наказание за несовершенное преступление...",
+        "shortDescription": "Пробивная официантка влюбляется в интеллигентного пианиста. Яркий тандем Олега Басилашвили и Людмилы Гурченко",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/1898899/f2f767fd-3fd1-4319-a95a-902fcd23963d/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/1898899/f2f767fd-3fd1-4319-a95a-902fcd23963d/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/1531675/2a00000176868cff776b960986cd76192bba/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/1531675/2a00000176868cff776b960986cd76192bba/x1000"
+        }
+    },
+    {
+        "id": 1228407,
+        "name": "Крик тишины",
+        "description": "Блокадный Ленинград, февраль 1942 года. Заканчивается самая страшная блокадная зима. Зина Воронова совсем отчаялась. Ее трёхлетний сын Митя ослабел, а карточки на хлеб уже отоварены на два дня вперед. Последняя надежда — эвакуация. Но с маленькими детьми не берут. Тогда женщина идет на чудовищный шаг — эвакуируется, оставив сына одного в промерзшей квартире. Во время налета мальчика спасает девочка-подросток Катя. Девочка выдает Митю за своего брата и обещает себе сделать все, чтобы Митя выжил. Несмотря ни на что.",
+        "shortDescription": "Двое детей пытаются выжить в блокадном Ленинграде. Пронзительная драма о глубине горя и силе любви",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/4774061/a3c9705f-e6cc-40d6-821f-ced1b04308f2/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/4774061/a3c9705f-e6cc-40d6-821f-ced1b04308f2/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/239697/2a0000016a5007a545f394e0cae67c3da7f1/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/239697/2a0000016a5007a545f394e0cae67c3da7f1/x1000"
+        }
+    },
+    {
+        "id": 7724,
+        "name": "Летят журавли",
+        "description": "Рассказ о людях, в чьи судьбы безжалостно вторглась война. Не все смогли с честью вынести это испытание. В центре киноповести - трагическая история двух влюбленных, которых разлучила война.",
+        "shortDescription": "Трагедия войны глазами солдатской невесты. Фильм, победивший в Каннах и изменивший советский кинематограф",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/1777765/93397e62-0402-4cf9-9b4a-e462fea91963/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/1777765/93397e62-0402-4cf9-9b4a-e462fea91963/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/223007/2a000001616b99441918599a9270da43eaee/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/223007/2a000001616b99441918599a9270da43eaee/x1000"
+        }
+    },
+    {
+        "id": 959606,
+        "name": "Работа без авторства",
+        "description": "Молодой художник Курт Барнерт сбегает из Восточной Германии в Западную, чтобы учиться живописи и свободно работать. Однако болезненное прошлое дает о себе знать.",
+        "shortDescription": "Художник переезжает в Западную Германию в поисках свободы творчества. Блестящая драма режиссера «Жизни других»",
+        "poster": {
+            "url": "https://image.openmoviedb.com/kinopoisk-images/1629390/5884c004-d733-418e-8e94-d50759eac02e/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-images/1629390/5884c004-d733-418e-8e94-d50759eac02e/x1000"
+        },
+        "backdrop": {
+            "url": "https://image.openmoviedb.com/kinopoisk-ott-images/223007/2a0000016d2986cc6338f85dfca0e781df0e/orig",
+            "previewUrl": "https://image.openmoviedb.com/kinopoisk-ott-images/223007/2a0000016d2986cc6338f85dfca0e781df0e/x1000"
+        }
+    },
+]
